bcf7c216078cf26f7e9a8d9d7aae7c6890b6db3a
Noah Pflaum <pflauming@gmail.com>
2024-10-16 15:20:11 -0600

    Adding back in the index.html

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # littlelemon
+
+## Routes
+```
+/restaurant/
+/restaurant/menu/
+/restaurant/menu/<int:pk>
+/restaurant/api-token-auth/
+/restaurant/booking/
+/restaurant/booking/tables/
+
+/auth/ for djoser
+```
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,4 +1,5 @@
 # from django.shortcuts import render
+from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, DestroyAPIView
@@ -9,6 +10,9 @@ from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 
 # Create your views here.    
+def index(request):
+    return render(request, 'index.html', {})
+
 class MenuItemView(ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
